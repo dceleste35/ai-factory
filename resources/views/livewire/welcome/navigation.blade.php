02d4041f2571299eefe3db2ea458e2401cd40b22
dceleste35 <145990752+dceleste35@users.noreply.github.com>
2024-12-29 18:51:57 +0100

    Implement authentification Fixes #13

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,11 +1,11 @@
-<nav class="flex flex-1 justify-end space-x-2">
+<nav class="flex flex-1 justify-end">
     @auth
         <flux:button href="{{ url('/home') }}" wire:navigate>{{ __('Home') }}</flux:button>
     @else
-        <flux:button href="{{ route('login') }}" wire:navigate>{{ __('Log in') }}</flux:button>
+        <flux:button href="{{ route('login') }}" wire:navigate variant="ghost">{{ __('Log in') }}</flux:button>
 
         @if (Route::has('register'))
-            <flux:button href="{{ route('register') }}" wire:navigate>
+            <flux:button href="{{ route('register') }}" wire:navigate variant="ghost">
                 {{ __('Register') }}
             </flux:button>
         @endif
